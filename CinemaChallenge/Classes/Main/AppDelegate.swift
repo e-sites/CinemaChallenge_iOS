c6e3d6ce78e45ad70cc5d75ae875f8095007e61b
Bas van Kuijck <bas@e-sites.nl>
2017-09-14 16:29:45 +0200

    Swift 4 / Xcode9

--- CinemaChallenge/Classes/Main/AppDelegate.swift
+++ CinemaChallenge/Classes/Main/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var mainViewController:MainViewController?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         mainViewController = MainViewController(nibName: MainViewController.nibName, bundle: Bundle.main)
         window?.rootViewController = mainViewController
--- CinemaChallenge/Classes/Main/MainViewController.swift
+++ CinemaChallenge/Classes/Main/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainViewController : UIViewController  {
+class MainViewController: UIViewController  {
 
     static let nibName = "MainView"
     
